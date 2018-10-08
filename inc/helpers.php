2335f500c41da2b2de70c8c61a3f1fed0bf44a35
Pierre Saïkali <dev@mosaika.fr>
2018-10-08 10:36:29 +0200

    Adding helpers class for utilities functions.

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,3 +1,28 @@
 <?php
 
-namespace MountainConqueror\Helpers;
+namespace MountainConqueror;
+
+/**
+ * Helpers class providing some utilities function
+ */
+class Helpers {
+	/**
+	 * Write in wp-content/debug.log for quick alternative to xDebugging
+	 *
+	 * @param string|array|object $log
+	 * @return void
+	 */
+	public static function debug( $log ) {
+		if ( true === WP_DEBUG && ! defined( 'WP_CURRENTLY_TESTING' ) ) {
+			error_log('--------------------------------- /!\ ' . __NAMESPACE__ . ' /!\ --------------------------------- ');
+
+			if ( is_array( $log ) || is_object( $log ) ) {
+				error_log( print_r( $log, true ) );
+			} else {
+				error_log( $log );
+			}
+
+			error_log('--------------------------------- /!\ ' . __NAMESPACE__ . ' /!\ --------------------------------- ');
+		}
+	}
+}
